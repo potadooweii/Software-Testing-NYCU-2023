dd6cd3f7f3cf75f569a9c4a140a1d3140227e177
potadoo <potadooweii@gmail.com>
2023-03-03 22:47:46 +0800

    [Lab2] first try

---
+++ Lab02/app_test.py
@@ -0,0 +1,37 @@
+import unittest
+from unittest.mock import Mock, patch
+from app import Application, MailSystem
+
+class ApplicationTest(unittest.TestCase):
+
+    @patch.object(Application, 'get_names')
+    def setUp(self, mock_get_names: Mock):
+        # stub
+        mock_get_names.return_value = (
+            ['William', 'Oliver', 'Henry', 'Liam'],
+            ['William', 'Oliver', 'Henry'],
+        )
+        self.app = Application()
+
+    # There should not be so many decorator here!
+    # the app.py could be refactored.
+    @patch.object(MailSystem, 'write')
+    @patch.object(MailSystem, 'send')
+    @patch.object(Application, 'get_random_person')
+    def test_app(self, mock_get_random: Mock, mock_send: Mock, mock_write: Mock):
+        # Test Application
+        mock_get_random.side_effect = (person for person in self.app.people)
+        selected_person = self.app.select_next_person()
+        self.assertEqual(selected_person, 'Liam')
+        print(f'{selected_person} selected')
+
+        # Test MailSystem
+        mock_write.side_effect = lambda name: 'Congrats, ' + name + '!'
+        mock_send.side_effect = lambda _, ctx: print(ctx)
+        self.app.notify_selected()
+        self.assertEqual(mock_write.call_count, len(self.app.selected))
+        self.assertEqual(mock_send.call_count, len(self.app.selected))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 2023 Spring NYCU Software Testing, lectured by Professor Shih-Kun Huang.  
 311551035 董柏葳  
 
-![example workflow](https://github.com/potadooweii/311551035-ST-2023/actions/workflows/Lab0-CI.yml/badge.svg)    
+![example workflow](https://github.com/potadooweii/311551035-ST-2023/actions/workflows/Lab0-CI.yml/badge.svg)
+
 ![example workflow](https://github.com/potadooweii/311551035-ST-2023/actions/workflows/Lab01-CI.yml/badge.svg)   
+
+![example workflow](https://github.com/potadooweii/311551035-ST-2023/actions/workflows/Lab02-CI.yml/badge.svg)   
+
