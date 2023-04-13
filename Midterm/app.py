fa57be8abb53a4fe091ecc9e77e015ce058cb32a
potadooweii <potadooweii@gmail.com>
2023-04-13 14:45:08 +0800

    [Midterm] first

---
+++ Midterm/app.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+
+TIMEOUT = 10
+
+options = Options()
+options.add_argument("--headless")
+options.add_argument("--window-size=1920,1080")
+options.add_argument("--disable-gpu")
+
+# launch browser
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()), options=options
+)
+
+driver.get("https://docs.python.org/3/tutorial/index.html")
+driver.maximize_window()
+
+# Q2-1
+# WebDriverWait(driver, TIMEOUT).until(EC.visibility_of_element_located((By.ID, "language_select")))
+# select = Select(driver.find_element(By.XPATH, '//*[@id="language_select"]'))
+# select.select_by_value('zh-tw')
+
+first_paragraph = WebDriverWait(driver, TIMEOUT).until(
+    lambda d: d.find_element(By.XPATH, '//*[@id="the-python-tutorial"]/p[1]')
+).text
+print(first_paragraph)
+
+# Q2-2
+search_box = WebDriverWait(driver, TIMEOUT).until(
+    lambda d: d.find_element(By.XPATH, '/html/body/div[2]/ul/li[11]/div/form/input[1]')
+)
+search_box.send_keys('class')
+driver.find_element(By.XPATH, '/html/body/div[2]/ul/li[11]/div/form/input[2]').click()
+
+for i in range(1, 6):
+    title = WebDriverWait(driver, TIMEOUT).until(
+        lambda d: d.find_element(By.XPATH, f'//*[@id="search-results"]/ul/li[{i}]/a')
+    ).text
+    print(title)
---
+++ Midterm/course_scheduling_system_test.py
@@ -0,0 +1,121 @@
+import unittest
+from unittest.mock import Mock, patch
+from course_scheduling_system import CSS
+
+
+class CSSTest(unittest.TestCase):
+    @patch.object(CSS, "check_course_exist")
+    def setUp(self, mock_check_course_exist: Mock):
+        # stub
+        pass
+
+    @patch.object(CSS, "check_course_exist")
+    def test_q1_1(self, mock_check_course_exist: Mock):
+        mock_check_course_exist.return_value = True
+        css = CSS()
+
+        with self.subTest():
+            course = ('ST', 'Monday', 1, 2)
+            ret = css.add_course(course)
+            # Check return value
+            self.assertTrue(ret)
+        
+            ret = css.get_course_list()
+            # Verify Result
+            self.assertEqual(len(ret), 1)
+            self.assertEqual(ret[0], course)
+
+    @patch.object(CSS, "check_course_exist")
+    def test_q1_2(self, mock_check_course_exist: Mock):
+        mock_check_course_exist.return_value = True
+        css = CSS()
+
+        with self.subTest():
+            course = ('ST', 'Monday', 1, 2)
+            ret = css.add_course(course)
+            self.assertTrue(ret)
+            ret = css.add_course(course)
+            self.assertFalse(ret)
+        
+            ret = css.get_course_list()
+            self.assertEqual(len(ret), 1)
+            self.assertEqual(ret[0], course)
+    
+    @patch.object(CSS, "check_course_exist")
+    def test_q1_3(self, mock_check_course_exist: Mock):
+        mock_check_course_exist.return_value = False
+        css = CSS()
+
+        with self.subTest():
+            course = ('ST', 'Monday', 1, 2)
+            ret = css.add_course(course)
+            # Check return value
+            self.assertFalse(ret)
+        
+            ret = css.get_course_list()
+            # Verify Result
+            self.assertEqual(len(ret), 0)
+
+    @patch.object(CSS, "check_course_exist")
+    def test_q1_4(self, mock_check_course_exist: Mock):
+        mock_check_course_exist.return_value = True
+        css = CSS()
+        
+        with self.subTest():
+            with self.assertRaises(TypeError):
+                course = ('ST', 'Monday', 2, 1)
+                ret = css.add_course(course)
+            with self.assertRaises(TypeError):
+                course = (1, 'Monday', 1, 2)
+                ret = css.add_course(course)
+            with self.assertRaises(TypeError):
+                course = ('ST', 'XXX', 1, 2)
+                ret = css.add_course(course)
+    
+    @patch.object(CSS, "check_course_exist")
+    def test_q1_5(self, mock_check_course_exist: Mock):
+        mock_check_course_exist.return_value = True
+        css = CSS()
+
+        with self.subTest():
+            course1 = ('ST', 'Monday', 1, 2)
+            course2 = ('DL', 'Tuesday', 1, 2)
+
+            css.add_course(course1)
+            css.add_course(course2)
+            css.remove_course(course2)
+
+            ret = css.get_course_list()
+            self.assertEqual(len(ret), 1)
+            self.assertEqual(ret[0], course1)
+            self.assertEqual(mock_check_course_exist.call_count, 3)
+
+            print('')
+            print(css)
+
+    @patch.object(CSS, "check_course_exist")
+    def test_q1_6(self, mock_check_course_exist: Mock):
+        mock_check_course_exist.return_value = True
+        css = CSS()
+
+        with self.subTest():
+            course = ('ST', 'Monday', 1, 2)
+
+            css.add_course(course)
+
+            bad_courses = [
+                ('DL', 'Monday', 1, 2)
+            ]
+
+            for bad_course in bad_courses:
+                ret = css.add_course(bad_course)
+                self.assertFalse(ret)
+                
+
+            css.remove_course(course)
+            self.assertFalse(css.remove_course(course))
+
+
+
+if __name__ == "__main__": # pragma no cover
+    unittest.main()
